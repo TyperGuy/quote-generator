74a5ecd87b5d8961285b57c40b40b798e387355b
RJ Tony <ray.developper@gmail.com>
2021-11-01 14:35:44 -0900

    feat : Get quote from API

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ async function getQuotefromAPI(){
   const apiUrl = 'http://api.forimastic.com/api/1.0/?method=getQuote?&lang=en&format=json';
 
   try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
     
   } catch (error) {
     console.log('Uppps, aconteceu algun erro',error)
