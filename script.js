06bfb786830906a2cdf012a6fb5fabad6dffa21c
RJ Tony <ray.developper@gmail.com>
2021-11-01 14:46:37 -0900

    fix (API) : CORS Policy acess control on API

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 async function getQuotefromAPI(){
 
+  const proxyUrl ='https://cors-anywhere.herokuapp.com/';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote?&lang=en&format=json';
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
     
