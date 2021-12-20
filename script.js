ba34cc145d3bff4556c815cacca0dcdda37d6ec4
TyperGuy <ray.developper@gmail.com>
2021-12-20 22:01:27 +0100

    feat : copy quote to trnasfere area

--- script.js
+++ script.js
@@ -3,30 +3,37 @@ const copyBtn = document.querySelector('#copyBtn');
 const sreBtn = document.querySelector('#shareBtn');
 const quoteText = document.querySelector('#quote');
 const quoteAuthor = document.querySelector('#author');
+const proxyUrl ='https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
 
 //get quote from API
 
 async function getQuotefromAPI(){
-
-  const proxyUrl ='https://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-
-  try {
-    const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    quoteText.innerText = data.quoteText;
-    quoteAuthor.innerText = data.quoteAuthor;
-    console.log(data)
-    
-    
+   try {
+      const response = await fetch(proxyUrl + apiUrl);
+      const data = await response.json();
+      quoteText.innerText = data.quoteText;
+      quoteAuthor.innerText = data.quoteAuthor;
+      console.log(data)
   } catch (error) {
-    console.log('Uppps, aconteceu algun erro',error)
-    getQuotefromAPI();
+      console.log('Uppps, aconteceu algun erro',error)
+      getQuotefromAPI();
   }
 
 }
 
+function copyTo(){
+ 
+    //window.clipboardData.setData("quote", quoteText.value);
+    navigator.clipboard.writeText(quoteText.textContent);
+  
+
+  
+}
+
 nextBtn.addEventListener('click', getQuotefromAPI);
+copyBtn.addEventListener('click', copyTo);
 
 
 
